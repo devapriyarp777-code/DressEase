13b06865d2f8c846543ec07f5848117abd21eb7a
devapriyarp777-code <devapriyarp777@gmail.com>
2026-02-28 12:17:05 +0530

    Add Dockerfile and optimize app for deployment

--- app.py
+++ app.py
@@ -215,5 +215,6 @@ def liked():
     ]
     return render_template("liked.html", liked_outfits=liked_outfits)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
